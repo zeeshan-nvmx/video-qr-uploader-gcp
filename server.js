a14f304752a9b08c90212cde508d401a3c753dac
Ashiqur Rahman Zeeshan <zeeshan.nvmx@gmail.com>
2024-04-25 20:55:02 +0600

    replace method replaced with split ( for gcp private key)

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const storage = new Storage({
   projectId: process.env.GCP_PROJECT_ID,
   credentials: {
     client_email: process.env.GCP_CLIENT_EMAIL,
-    private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.GCP_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
   },
 })
 
